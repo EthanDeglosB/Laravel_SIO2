e209e9dce9124c94471c42de13f6bbf6e4a95e5a
Ethan <ethan.deglos-bizouarn@business-school.academy>
2024-10-01 12:08:29 +0200

    affichage de tout les objets

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Ore;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,10 +15,36 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
+        /*
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+        */
+
+        $ore1 = new Ore();
+        $ore1->price = '299.99';
+        $ore1->label = 'thorium';
+        $ore1->desc = 'rare';
+        $ore1->date_ = '1586-05-24';
+        $ore1->image_path = 'img/thorium_ore.jpg';
+        $ore1->save();
+
+
+        $ore2 = new Ore();
+        $ore2->price = '459.99';
+        $ore2->label = 'truesilver';
+        $ore2->desc = 'brillant';
+        $ore2->date_ = '1456-12-04';
+        $ore2->image_path = 'img/truesilver_ore.jpg';
+        $ore2->save();
+
+        $ore3 = new Ore();
+        $ore3->price = '1599.99';
+        $ore3->label = 'saronite';
+        $ore3->desc = 'Sang de DTA';
+        $ore3->date_ = '0186-05-15';
+        $ore3->image_path = 'img/saronite_ore.jpg';
+        $ore3->save();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,20 @@ Route::get('/', function () {
 });
 
 Route::get('/home', function(){
-    return view('home');
+    $username = 'Paul';
+
+/*    return view('home',[
+        'name' => $username,
+        ]);*/
+    return view('home',compact('username'));
 });
+
+Route::get('/Ores', function(){
+    $all_ores = \App\Models\Ore::all();
+    return view('index', compact('all_ores'));
+});
+/*
+Route::get('/Ores/',function(){
+    $specific_ore = \App\Models\Ore::find($id);
+});
+*/
